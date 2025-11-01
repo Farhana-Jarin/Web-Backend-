30afd8fd69ec16e4f74b7f3325ecf0300af8b94d
Farhana-Jarin <jarinakther1303@gmail.com>
2025-11-01 15:48:12 +0600

    Add files via upload

---
+++ coco.js
@@ -0,0 +1,33 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/orders", orderRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Backend is working!");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Mosgoose is connected");
+  })
+  .catch((error) => {
+    console.log("Something went wrong", error);
+  });
+
+app.listen(8000, () => {
+  console.log("Running in http://localhost:8000");
+});
