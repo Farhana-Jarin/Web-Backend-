bb14b6f2b9e6e08cb9f91cf797dc212cf4f96ea2
Emtiaz Ahmed <160145834+emtiaz-13@users.noreply.github.com>
2025-11-02 18:13:08 +0600

    Update coco.js

--- coco.js
+++ coco.js
@@ -4,16 +4,18 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import cartRoutes from "./routes/cartRoutes.js";
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/carts", cartRoutes);
 
 app.get("/", (req, res) => {
   res.send("Backend is working!");
@@ -31,3 +33,4 @@ mongoose
 app.listen(8000, () => {
   console.log("Running in http://localhost:8000");
 });
+
