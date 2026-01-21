c36056f3c928a0d883982934ccd5d549db5a59f4
Emtiaz Ahmed <160145834+emtiaz-13@users.noreply.github.com>
2026-01-22 00:14:02 +0600

    Add review, payment, and address routes

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ import productRoutes from "./routes/productRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import cartRoutes from "./routes/cartRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
+import reviewRoutes from "./routes/reviewRoute.js";
+import paymentRoutes from "./routes/paymentRoute.js";
+import addressRoutes from "./routes/addressRoute.js";
 
 dotenv.config();
 const app = express();
@@ -19,6 +22,10 @@ app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/carts", cartRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/reviews", reviewRoutes);
+app.use("/api/payments", paymentRoutes);
+app.use("/api/addresses", addressRoutes);
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Backend is working!");
@@ -38,3 +45,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Runnning in http://localhost:${PORT}`);
 });
+
